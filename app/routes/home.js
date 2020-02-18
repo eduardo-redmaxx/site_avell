43899395fa5dfc9497fd3d3abf60765372745207
eduardo-redmaxx <61057672+eduardo-redmaxx@users.noreply.github.com>
2020-02-18 14:38:53 -0400

    alteração_do_banco_senha

--- app/routes/home.js
+++ app/routes/home.js
@@ -7,8 +7,10 @@ module.exports = function(app){
         const config = {
             host: 'localhost',
             user: 'postgres',     
-            password: 'controle',
-            database: 'site_avell'
+            password: 'MudarAgora',
+            database: 'db_agente_avell'            
+            // password: 'controle',
+            // database: 'site_avell'
         };
 
         const client = new pg.Client(config);
